ca91167adfecd6afc2a025afcacb5e58a93165d8
Joe Corall <jjc223@lehigh.edu>
2023-06-13 13:06:40 -0400

    Allow fetching results from a directory listing

--- cmd/arxiv.go
+++ cmd/arxiv.go
@@ -10,7 +10,9 @@ import (
 	"net/url"
 	"os"
 	"strconv"
+	"time"
 
+	"github.com/lehigh-university-libraries/papercut/internal/utils"
 	"github.com/lehigh-university-libraries/papercut/pkg/arxiv"
 	"github.com/spf13/cobra"
 )
@@ -29,65 +31,91 @@ var (
 
 Thank you to arXiv for use of its open access interoperability.`,
 		Run: func(cmd *cobra.Command, args []string) {
-			if query == "" && ids == "" {
-				log.Fatal("query or ids required.")
-			}
+			queries := []string{}
 
-			params := url.Values{}
-			if query != "" {
-				params.Set("search_query", query)
-			}
-			if ids != "" {
-				params.Set("id_list", ids)
-			}
-
-			params.Set("start", strconv.Itoa(start))
-			params.Set("max_results", strconv.Itoa(results))
-
-			url, err := cmd.Flags().GetString("url")
+			dl, err := cmd.Flags().GetString("directory-listing")
 			if err != nil {
 				log.Fatal(err)
 			}
-			apiURL := fmt.Sprintf("%s?%s", url, params.Encode())
-
-			log.Printf("Accessing %s\n", apiURL)
-
-			resp, err := http.Get(apiURL)
-
-			if err != nil {
-				fmt.Println("Error requesting XML data:", err)
-				return
-			}
-			defer resp.Body.Close()
-
-			body, err := ioutil.ReadAll(resp.Body)
-			if err != nil {
-				fmt.Println("Error reading response body:", err)
-				return
+			if dl != "" {
+				if query != "" || ids != "" {
+					log.Fatal("query or ids can not be used with the directory listing option.")
+				}
+				queries, err = utils.FetchEmails(dl)
+				if err != nil {
+					log.Fatal(err)
+				}
 			}
 
-			var result arxiv.Feed
-			err = xml.Unmarshal(body, &result)
-			if err != nil {
-				fmt.Println("Error:", err)
-				return
+			if query != "" {
+				queries = append(queries, query)
+			}
+
+			if len(queries) == 0 && ids == "" {
+				log.Fatal("query or ids required.")
 			}
 
 			wr := csv.NewWriter(os.Stdout)
 
 			// header
-			wr.Write([]string{"id", "published", "updated", "title", "doi", "pdf"})
+			wr.Write([]string{"id", "published", "updated", "title", "doi", "pdf", "query"})
 
-			for _, e := range result.Entries {
-				wr.Write([]string{
-					e.ID,
-					e.Published.String(),
-					e.Updated.String(),
-					e.Title,
-					e.DOI,
-					e.PDF,
-				})
-				wr.Flush()
+			for _, query := range queries {
+
+				params := url.Values{}
+				if query != "" {
+					params.Set("search_query", query)
+				}
+				if ids != "" {
+					params.Set("id_list", ids)
+				}
+
+				params.Set("start", strconv.Itoa(start))
+				params.Set("max_results", strconv.Itoa(results))
+
+				url, err := cmd.Flags().GetString("url")
+				if err != nil {
+					log.Fatal(err)
+				}
+				apiURL := fmt.Sprintf("%s?%s", url, params.Encode())
+
+				log.Printf("Accessing %s\n", apiURL)
+
+				resp, err := http.Get(apiURL)
+				if err != nil {
+					fmt.Println("Error requesting XML data:", err)
+					return
+				}
+				defer resp.Body.Close()
+
+				body, err := ioutil.ReadAll(resp.Body)
+				if err != nil {
+					fmt.Println("Error reading response body:", err)
+					return
+				}
+
+				var result arxiv.Feed
+				err = xml.Unmarshal(body, &result)
+				if err != nil {
+					fmt.Println("Error:", err)
+					return
+				}
+
+				for _, e := range result.Entries {
+					wr.Write([]string{
+						e.ID,
+						e.Published.String(),
+						e.Updated.String(),
+						e.Title,
+						e.DOI,
+						e.PDF,
+						query,
+					})
+					wr.Flush()
+				}
+
+				log.Println("Pausing between requests. arXiv requests a three second delay between API requests...")
+				time.Sleep(3 * time.Second)
 			}
 
 		},
@@ -102,4 +130,5 @@ func init() {
 	arxivCmd.Flags().StringVarP(&ids, "ids", "i", "", "The arXiv API url")
 	arxivCmd.Flags().IntVarP(&start, "start", "s", 0, "The offset")
 	arxivCmd.Flags().IntVarP(&results, "results", "r", 10, "The number of results to return in a response")
+	arxivCmd.Flags().String("directory-listing", "", "URL to a web page listing faculty")
 }
